7821e6dbb543e87f580b121fd57ef9109f182a61
judy0131 <xiaojie.zhu@163.com>
2019-07-29 15:59:08 +0800

    add status.sh

---
+++ status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#ps term
+function psterm()
+{
+        [ ${#} -eq 0 ] && echo "usage: $FUNCNAME STRING" && return 0
+        local pid 
+        pid=$(ps ax | grep "$1" | grep -v grep | awk '{ print $1 }')
+	if [ $pid != "" ]
+	then
+		echo "server is Running!"
+		port=$(netstat -nltp | grep $pid | awk '{print $4}')
+		echo "$port"
+	else
+		echo "Server is Stopped!"
+	fi
+}
+#ps grep
+#ps aux | grep "piflow.api.Main"| grep -v 'grep'
+psterm piflow.api.Main 
