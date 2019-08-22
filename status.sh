917c466c38e607ecb2071b2802e8fdd34f9f2568
judy0131 <xiaojie.zhu@163.com>
2019-08-22 17:43:32 +0800

    modify script

--- status.sh
+++ status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #ps term
-function psterm()
+function check_status()
 {
         [ ${#} -eq 0 ] && echo "usage: $FUNCNAME STRING" && return 0
         local pid 
         pid=$(ps ax | grep "$1" | grep -v grep | awk '{ print $1 }')
-	if [ $pid != "" ]
+	if [[ "" -ne $pid ]]
 	then
 		echo "server is Running!"
 		port=$(netstat -nltp | grep $pid | awk '{print $4}')
@@ -16,4 +16,4 @@ function psterm()
 }
 #ps grep
 #ps aux | grep "piflow.api.Main"| grep -v 'grep'
-psterm piflow.api.Main 
+check_status piflow.api.Main 
